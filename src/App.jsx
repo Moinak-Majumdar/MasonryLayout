500faa858694b9127ee0e88de06dcaaa8d392ffe
TheNight-Owl <120689263+TheNight-Owl@users.noreply.github.com>
2024-04-10 19:30:33 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div id='main-box' className='columns-1 md:columns-2 lg:columns-3 xl:columns-4 w-full my-5 mx-auto gap-x-5'>
+        <div className='w-full break-inside-avoid mb-2'><img className='w-full rounded-xl' src="https://picsum.photos/200/400" alt="" /></div>
+        <div className='w-full break-inside-avoid mb-2'><img className='w-full rounded-xl' src="https://picsum.photos/300/600" alt="" /></div>
+        <div className='w-full break-inside-avoid mb-2'><img className='w-full rounded-xl' src="https://picsum.photos/200/500" alt="" /></div>
+        <div className='w-full break-inside-avoid mb-2'><img className='w-full rounded-xl' src="https://picsum.photos/600/400" alt="" /></div>
+        <div className='w-full break-inside-avoid mb-2'><img className='w-full rounded-xl' src="https://picsum.photos/200/600" alt="" /></div>
+        <div className='w-full break-inside-avoid mb-2'><img className='w-full rounded-xl' src="https://picsum.photos/200/800" alt="" /></div>
+        <div className='w-full break-inside-avoid mb-2'><img className='w-full rounded-xl' src="https://picsum.photos/300/500" alt="" /></div>
+        <div className='w-full break-inside-avoid mb-2'><img className='w-full rounded-xl' src="https://picsum.photos/400/300" alt="" /></div>
+        <div className='w-full break-inside-avoid mb-2'><img className='w-full rounded-xl' src="https://picsum.photos/500/200" alt="" /></div>
+        <div className='w-full break-inside-avoid mb-2'><img className='w-full rounded-xl' src="https://picsum.photos/600/300" alt="" /></div>
+      </div>
+    </>
+  )
+}
+
+export default App
