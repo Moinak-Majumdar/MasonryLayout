8f3c4567bb2b84006730cb780dc26e6188b0b226
TheNight-Owl <arkadyutidas122@gmail.com>
2024-06-01 14:36:11 +0530

    Added Heading

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import ImgGrid from "./components/ImgGrid"
 
 function App() {
   return (
-    <main className="px-4 md:px-10 lg:px-16 xl:px-36 2xl:px-44 min-h-screen flex bg-slate-800">
+    <main className="px-4 md:px-10 lg:px-16 xl:px-36 2xl:px-44 min-h-screen flex flex-col bg-slate-800">
+    <p className="text-slate-200 text-2xl z-10 text-center mt-4" >My masonry Layout</p>
       <ImgGrid />
       <Background />
     </main>
